22615f02dfd7cd33a9a291118d0b3c8088d5b9d2
qihg <18072753320@189.com>
2020-05-08 16:09:26 +0800

    两数之和

---
+++ go/two_sum_test.go
@@ -0,0 +1,41 @@
+/**
+题目：[1]两数之和
+*/
+package go_code
+
+import (
+	"reflect"
+	"testing"
+)
+
+type IO1 struct {
+	in1 []int
+	in2 int
+	out []int
+}
+
+func Test1(t *testing.T) {
+	tc := map[string]IO1{
+		"1": IO1{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		"2": IO1{[]int{2, 7, 2, 15}, 4, []int{0, 2}},
+		"3": IO1{[]int{1, 2, 2, 6}, 4, []int{1, 2}},
+	}
+
+	for k, v := range tc {
+		out := twoSum(v.in1, v.in2)
+		if !reflect.DeepEqual(out, v.out) {
+			t.Errorf("case-%v:except answer: [%v], get answer: [%v]", k, v.out, out)
+		}
+	}
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
