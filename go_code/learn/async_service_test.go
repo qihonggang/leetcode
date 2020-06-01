c79042a14255c01aa850d83c96914db023927e56
qihg <18072753320@189.com>
2020-06-01 11:02:05 +0800

    并发

---
+++ go_code/learn/async_service_test.go
@@ -0,0 +1,40 @@
+package learn
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on sonmething else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsynService() chan string {
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+
+func TestAsynService(t *testing.T) {
+	retCh := AsynService()
+	otherTask()
+	fmt.Println(<-retCh)
+}
