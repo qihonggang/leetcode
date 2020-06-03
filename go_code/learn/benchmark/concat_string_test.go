38655dd7f6fc31bfc26048a5069ff06b47221cf6
qihg <18072753320@189.com>
2020-06-03 15:33:47 +0800

    测试练习

---
+++ go_code/learn/benchmark/concat_string_test.go
@@ -0,0 +1,15 @@
+package benchmark
+
+import "testing"
+
+func BenchmarkStringByBytesBuffer(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret := ""
+		for _, elem := range elems {
+			ret += elem
+		}
+	}
+	b.StopTimer()
+}
---
+++ go_code/learn/functions.go
@@ -0,0 +1,5 @@
+package learn
+
+func square(op int) int {
+	return op * op
+}
