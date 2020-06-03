ff1ef3f98614e66ee9da3e623f2d2a186ee21aeb
祁宏钢 <915875033@qq.com>
2020-06-03 17:21:32 +0800

    反射

---
+++ go_code/learn/reflect_test.go
@@ -0,0 +1,12 @@
+package learn
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTypeAndValue(t *testing.T) {
+	var f int64 = 10
+	t.Log(reflect.TypeOf(f), reflect.ValueOf(f))
+	t.Log(reflect.ValueOf(f).Type())
+}
