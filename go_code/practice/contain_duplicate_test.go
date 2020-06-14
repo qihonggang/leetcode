03e733a71070a1da121392a236f62e465fcd52bb
qihonggang <sxxzqhg@163.com>
2020-06-14 23:54:03 +0800

    存在重复元素

---
+++ go_code/practice/contain_duplicate_test.go
@@ -0,0 +1,24 @@
+/**
+	题目：存在重复元素
+ */
+package practice
+
+import "testing"
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		} else {
+			m[v] = 1
+		}
+	}
+	return false
+}
+
+func TestContanisDuplicate(t *testing.T) {
+	arr := []int{1,2,4,3}
+	result := containsDuplicate(arr)
+	t.Log(result)
+}
