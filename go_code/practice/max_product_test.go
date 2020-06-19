6c99a829953e83095c6890835f6c8896fc90b381
qihonggang <sxxzqhg@163.com>
2020-06-19 17:45:09 +0800

    乘积最大子数组

---
+++ go_code/practice/max_product_test.go
@@ -0,0 +1,33 @@
+package practice
+
+import "testing"
+
+func maxProduct(nums []int) int {
+	maxF, minF, ans := nums[0], nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		mx, mn := maxF, minF
+		maxF = max(mx * nums[i], max(mn * nums[i], nums[i]))
+		minF = min(mx * nums[i], min(mn * nums[i], nums[i]))
+		ans = max(maxF, ans)
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if (x > y) {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if (x < y){
+		return x
+	}
+	return y
+}
+
+func TestMaxProduct(t *testing.T){
+	nums := []int{5, 30, -3, 4, -3}
+	t.Log(maxProduct(nums))
+}
