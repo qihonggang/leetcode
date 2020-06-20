b6bb2a1d6ec15271b5f085f163db75d14280317e
qihonggang <sxxzqhg@163.com>
2020-06-20 13:55:01 +0800

    chat

---
+++ go_code/demo/chat/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err  != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
