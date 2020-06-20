500ecf2000d8204c780fcda7b38aab92b211b372
qihonggang <sxxzqhg@163.com>
2020-06-20 18:40:55 +0800

    子字符串

---
+++ go_code/practice/strStr_test.go
@@ -0,0 +1,32 @@
+package practice
+
+/**
+	给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从 0 开始)。如果不存在，则返回 -1。
+ */
+
+import "testing"
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	var i, j int
+	for i = 0; i < len(haystack)- len(needle) + 1; i++ {
+		for j = 0; j < len(needle); j++ {
+			if haystack[i + j] != needle[j] {
+				break
+			}
+		}
+		if len(needle) == j {
+			return i
+		}
+	}
+	return -1
+}
+
+func TestStrStr(t *testing.T){
+	haystack := "abcdefg"
+	needle := "bcd"
+	i := strStr(haystack, needle)
+	t.Log(i)
+}
