4966993779ab3ae2a81cece47a988e8f8f75d206
qihonggang <sxxzqhg@163.com>
2020-06-24 23:55:48 +0800

    寻找旋转排序数组中的最小值

---
+++ go_code/practice/find_min_test.go
@@ -0,0 +1,22 @@
+package practice
+
+import "testing"
+
+func findMin(nums []int) int {
+	left :=0
+	right :=len(nums)-1
+	for left<right{
+		mid :=left+(right-left)>>1
+		if nums[mid]>nums[right]{
+			left = mid+1
+		}else {
+			right= right-1
+		}
+	}
+	return nums[left]
+}
+
+func TestFindMin(t *testing.T){
+	nums := []int{3,4,5,1,2}
+	t.Log(findMin(nums))
+}
