5f68ea2995484ccb7c0b4011468e07f5d7885a74
qihonggang <sxxzqhg@163.com>
2020-07-08 09:40:46 +0800

    爬楼梯

---
+++ go_code/climb_staits_test.go
@@ -0,0 +1,20 @@
+package go_code
+
+import "testing"
+
+/*
+70. 爬楼梯
+ */
+func climbStairs(n int) int {
+	p, q, r := 0, 0, 1
+	for i := 1; i <= n; i++{
+		p = q
+		q = r
+		r = q + p
+	}
+	return r
+}
+
+func TestClimbStairs(t *testing.T){
+	t.Log(climbStairs(3))
+}
