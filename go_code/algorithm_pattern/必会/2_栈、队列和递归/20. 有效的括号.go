c21aeb3198cc4a0d289fe654b19e54334ca471b8
qihonggang <sxxzqhg@163.com>
2021-02-19 19:36:54 +0800

    每日一练

--- go_code/algorithm_pattern/必会/2_栈、队列和递归/20. 有效的括号.go
+++ go_code/algorithm_pattern/必会/2_栈、队列和递归/20. 有效的括号.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 /**
 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
 
@@ -11,3 +17,35 @@ package main
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/valid-parentheses
  */
+
+func isValid(s string) bool {
+	n := len(s)
+	if n % 2 == 1 {
+		return false
+	}
+	stack := []string{}
+	for _, ch := range s {
+		c := string(ch)
+		if c == "{" || c == "(" || c == "[" {
+			stack = append(stack, c)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack) - 1]
+			if top == "(" && c == ")" || top == "[" && c == "]" || top == "{" && c == "}" {
+				stack = stack[:len(stack) - 1]
+			} else {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	input, _, _ := reader.ReadLine()
+	in := string(input)
+	fmt.Print(isValid(in))
+}
--- go_code/algorithm_pattern/必会/2_栈、队列和递归/32. 最长有效括号.go
+++ go_code/algorithm_pattern/必会/2_栈、队列和递归/32. 最长有效括号.go
@@ -1,4 +1,6 @@
 package main
 /**
 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+
+https://leetcode-cn.com/problems/longest-valid-parentheses/
  */
