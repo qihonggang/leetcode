fa8c5f60ecc24762ae4c5f2fe9dbe78f1d2b083f
qihonggang <sxxzqhg@163.com>
2021-02-25 16:54:23 +0800

    每日一练

--- go_code/algorithm_pattern/必会/2_栈、队列和递归/239. 滑动窗口最大值.go
+++ go_code/algorithm_pattern/必会/2_栈、队列和递归/239. 滑动窗口最大值.go
@@ -9,3 +9,32 @@ package main
 链接：https://leetcode-cn.com/problems/sliding-window-maximum
  */
 
+func maxSlidingWindow(nums []int, k int) []int {
+	// 单调双端队列
+	q := []int{}
+	
+	// 单调双端队列 push 方法
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q) - 1]] {
+			q = q[:len(q) - 1]
+		}
+		q = append(q, i)
+	}
+	// 0-k
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+	// k-n
+	n := len(nums)
+	ans := make([]int, 1, n - k + 1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i - k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
+	}
+	return ans
+}
+
